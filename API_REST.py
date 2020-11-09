e340d807a24fc2b9c478dad7c0e3dfa11ceadf40
Aitor <tornay13@hotmail.com>
2020-11-09 11:21:35 +0100

    añadida libreria gunicorn

--- API_REST.py
+++ API_REST.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+import gunicorn
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Aitor:Cerdanyola26@localhost/Clientes'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+CORS(app)
 
 db = SQLAlchemy(app)
 mm = Marshmallow(app)
